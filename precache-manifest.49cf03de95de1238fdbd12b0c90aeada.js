ed73ebb18dd933528fd9180fe6dfa6a7dffe525b
Daniel Corcoran <danielc.92@outlook.com>
2019-07-10 17:58:24 +1000

    Updates

---
+++ precache-manifest.49cf03de95de1238fdbd12b0c90aeada.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ea2ecd96062c9930752698dd2e410aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "226a5bac09452632d66c",
+    "url": "/static/js/main.52165002.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
