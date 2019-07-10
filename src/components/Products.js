290528624f2068afd8d983599ab654b43d02d6c5
Daniel Corcoran <danielc.92@outlook.com>
2019-07-10 15:38:08 +1000

    Added map to products component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
+import ProductItem from './ProductItem';
 
 export default class Products extends Component {
     render() {
         return (
             <div>
                 <h3>Products</h3>
+                {this.props.products.map((item, index) => {
+                    return (
+                        <ProductItem item={item} index={index}/>
+                    )
+                })}
             </div>
         )
     }
