0fa822a5b17bf98c4038ea588837f196e926b8e3
Daniel Corcoran <danielc.92@outlook.com>
2019-07-10 18:00:27 +1000

    Updates

---
+++ precache-manifest.71b8923d2865020b527fe44e23984ec6.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de4d5a0fc51a4708d262ad6e604ca738",
+    "url": "/cubic-exercise/index.html"
+  },
+  {
+    "revision": "123fb41c3fb46a6d1e24",
+    "url": "/cubic-exercise/static/js/2.a5adb38c.chunk.js"
+  },
+  {
+    "revision": "226a5bac09452632d66c",
+    "url": "/cubic-exercise/static/js/main.52165002.chunk.js"
+  },
+  {
+    "revision": "abff6327056c864b3263",
+    "url": "/cubic-exercise/static/js/runtime~main.e79297a7.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.49cf03de95de1238fdbd12b0c90aeada.js"
+  "/cubic-exercise/precache-manifest.71b8923d2865020b527fe44e23984ec6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cubic-exercise/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
